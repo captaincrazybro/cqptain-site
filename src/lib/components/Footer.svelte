2ad94db1ad8263a3dd037a07c1b4f0958f41c8fb
Levi Wenger <myeyesareonmizar@gmail.com>
2023-05-09 12:22:42 -0400

    Fixed some issues with the heights of projects and enhanced the Footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,10 +1,11 @@
 <style>
     .footer {
         background-color: white;
-        position: absolute;
+        position: fixed;
         bottom: 0;
         padding: 10px 0;
         width: 100%;
+        box-shadow: 0 6px 20px 10px rgba(0, 0, 0, 0.19);
     }
 </style>
 
